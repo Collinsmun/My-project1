f55af754ea767b5d3bc3a2dddc015bf53df79f21
Collins Mundia <collins.mwangi@student.moringaschool.com>
2022-06-23 02:31:56 +0300

    second commit

---
+++ index.js
@@ -0,0 +1,40 @@
+// store functions and variables
+let weather = {
+    "apikey":"6b26af17b803a28cbf74b4f0bce67f51",
+    fetchWeather : function(city){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=" + this.apikey
+        ).then((res) => res.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data){
+        const {name}= data;
+        const {icon, description}= data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed}=data.wind;
+        // console.log(name, icon, description, temp, humidity, speed)
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src="https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText= temp + "°C";
+        document.querySelector(".humidity").innerText="Humidity "+ humidity + "%";
+        document.querySelector(".wind").innerText="Wind speed "+ speed + "km/h";
+        document.body.style.backgroundImage="url('https://source.unsplash.com/random?'" + name + ")"
+    },
+
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+
+document.querySelector(".search button").addEventListener("click", function(){
+    weather.search();
+
+})
+
+document.querySelector(".search-bar").addEventListener("keyup", function(e){
+    if(e.key==="Enter"){
+        weather.search();
+    }
+})
+
+weather.fetchWeather("Nairobi");
