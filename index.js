9facd89c08e7aef61b5ebd2273cc45c7d50d61f0
Collins Mundia <collins.mwangi@student.moringaschool.com>
2022-06-24 14:41:42 +0300

    Add icon

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let weather = {
         const {speed}=data.wind;
         // console.log(name, icon, description, temp, humidity, speed)
         document.querySelector(".city").innerText = "Weather in " + name;
-        (".icon").src="https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".icon").src="https://openweathermap.org/img/wn/" + icon + "@2x.png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText= temp + "°C";
         document.querySelector(".humidity").innerText="Humidity "+ humidity + "%";
